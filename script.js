f43a092ac4e8a9b6ea7ef2dc38e341d25e67572e
Kevin Kinner <kevin-kinner@web.de>
2022-08-14 10:59:12 +0200

    Browser Version message + list od commands

--- script.js
+++ script.js
@@ -1,5 +1,22 @@
 const start = (() => {
 
+    navigator.sayswho = (function() {
+        var ua = navigator.userAgent;
+        var tem;
+        var M = ua.match(/(opera|chrome|safari|firefox|msie|trident(?=\/))\/?\s*(\d+)/i) || [];
+        if (/trident/i.test(M[1])) {
+            tem = /\brv[ :]+(\d+)/g.exec(ua) || [];
+            return 'IE ' + (tem[1] || '');
+        }
+        if (M[1] === 'Chrome') {
+            tem = ua.match(/\b(OPR|Edge)\/(\d+)/);
+            if (tem != null) return tem.slice(1).join(' ').replace('OPR', 'Opera');
+        }
+        M = M[2] ? [M[1], M[2]] : [navigator.appName, navigator.appVersion, '-?'];
+        if ((tem = ua.match(/version\/(\d+)/i)) != null) M.splice(1, 1, tem[1]);
+        return M.join(' ');
+    })();
+
     function appendInputLine() {
         let cliWrapper = document.getElementById("cliWrapper");
         let prefix = document.createElement("span");
@@ -14,22 +31,37 @@ const start = (() => {
         lastChild.select();
         lastChild.addEventListener("keyup", ({ key }) => {
             if (key === "Enter") {
-                help();
-                lastChild.replaceWith(lastChild);
+                spellchecker(lastChild.value);
             }
         })
     }
 
-    function help() {
+    function spellchecker(lastChild) {
         let cliWrapper = document.getElementById("cliWrapper");
         div = document.createElement("div");
-        div.textContent = "contact - list possible contact"
+        div.setAttribute("style", "white-space:pre;");
+        switch (lastChild) {
+            case "help":
+                div.textContent = "help         - list of commands\r\ncontact      - opens deafault mail programm\r\nprj          - list of projects\r\nop <value>   - opens selected project in new tab\r\nhub          - opens GitHub profile in a new Tab\r\nbrd          - if you are bored\r\nclear        - reset cli\r\nexit         - close tab";
+                break;
+
+            default:
+                div.textContent = lastChild + " is not a recognized command"
+                break;
+        }
         cliWrapper.appendChild(div);
+
+
+
+
         appendInputLine();
     }
 
 
 
-    appendInputLine();
+    p = document.createElement("p");
+    p.textContent = "Running on " + navigator.sayswho;
+    document.getElementById("header").appendChild(p);
 
+    appendInputLine();
 })();
