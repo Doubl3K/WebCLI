91cd8df3c80c316f0e7ba7fcb9992381c0146193
Kevin Kinner <kevin-kinner@web.de>
2022-08-14 09:53:09 +0200

    fixed multiple input through one line

--- script.js
+++ script.js
@@ -1,16 +1,33 @@
 const start = (() => {
+    let cliWrapper = document.getElementById("cliWrapper");
+
     function appendInputLine() {
-        let cliWrapper = document.getElementById("cliWrapper");
         let prefix = document.createElement("span");
         let userInput = document.createElement("INPUT");
-        userInput.attributes = "rows=1";
         prefix.textContent = "DWB:/";
         cliWrapper.appendChild(prefix);
         cliWrapper.appendChild(userInput);
         let lastChild = cliWrapper.lastChild;
+
+        //Place cursor in last line and listen for Return press
         lastChild.focus();
         lastChild.select();
+        lastChild.addEventListener("keyup", ({ key }) => {
+            if (key === "Enter") {
+                help();
+                lastChild.replaceWith(lastChild);
+            }
+        })
     }
+
+    function help() {
+        p = document.createElement("p");
+        p.textContent = "contact - list possible contact"
+        cliWrapper.appendChild(p);
+    }
+
+
+
     appendInputLine();
-    document.addEventListener("Enter", appendInputLine);
+
 })();
